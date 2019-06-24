186d877218a9deef62288cfaaa6f18f4fda90b57
lixingyu <lixingyu@renaissance-era.com>
2019-06-24 20:25:48 +0800

    添加mamarkdown在admin中

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from markdownx.admin import MarkdownxModelAdmin
+from .models import User
+from .models import Article
+
+admin.site.register(Article, MarkdownxModelAdmin)
